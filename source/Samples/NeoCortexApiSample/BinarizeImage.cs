ed11e61db64bffb47a16f1dd69f0c2048838a333
Avradip Mazumdar <rontymazumdar@gmail.com>
2025-02-10 23:10:05 +0100

    Adding the BinariImage functionality as method

---
+++ source/Samples/NeoCortexApiSample/BinarizeImage.cs
@@ -0,0 +1,45 @@
+﻿using Daenet.ImageBinarizerLib.Entities;
+using Daenet.ImageBinarizerLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+namespace NeoCortexApiSample
+{
+    internal class ImageBinarizationUtils
+    {
+        /// <summary>
+        /// Converts an image to a binarized format and saves it as a .txt file.
+        /// </summary>
+        /// <param name="imageWidth">Width of the binarized image.</param>
+        /// <param name="imageHeight">Height of the binarized image.</param>
+        /// <param name="destinationPath">The file path where the binarized image will be saved.</param>
+        /// <param name="imagePath">Path to the input image file.</param>
+        /// <returns>Path of the saved binarized image in .txt format.</returns>
+        public static string BinarizeImages(int imageWidth, int imageHeight, string destinationPath, string imagePath)
+        {
+            string binaryImage = $"{destinationPath}.txt";
+
+            // Delete existing file if it exists
+            if (File.Exists(binaryImage))
+                File.Delete(binaryImage);
+
+            // Initialize and run the binarizer
+            ImageBinarizer imageBinarizer = new ImageBinarizer(new BinarizerParams
+            {
+                RedThreshold = 200,
+                GreenThreshold = 200,
+                BlueThreshold = 200,
+                ImageWidth = imageWidth,
+                ImageHeight = imageHeight,
+                InputImagePath = imagePath,
+                OutputImagePath = binaryImage
+            });
+
+            imageBinarizer.Run();
+
+            return binaryImage;
+        }
+    }
+}
