1336803ae5730e0b74bb0645c2f6cb9e55021e8b
Anoushka1408 <anoushkapiplai@gmail.com>
2025-02-13 20:50:53 +0100

    Adding KNN Classifier with IClassifier interface

---
+++ source/Samples/NeoCortexApiSample/knnClassifier.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NeoCortexApi.Entities;
+using NeoCortexApi.Classifiers;
+using NeoCortexApiSample;
+
+namespace NeoCortexApiSample
+{
+    public class KnnImageClassifier : IClassifier<Cell, string>
+    {
+        private readonly KNeighborsClassifier<string, ComputeCycle> _knnClassifier = new KNeighborsClassifier<string, ComputeCycle>();
+
+        public void Learn(string key, Cell[] activeCells)
+        {
+            _knnClassifier.Learn(key, activeCells);
+        }
+
+        public List<PredictedResult<string>> GetPredictedInputValues(Cell[] predictiveCells, int k)
+        {
+            var predictions = _knnClassifier.GetPredictedInputValues(predictiveCells, (short)k);
+            return predictions.Select(p => new PredictedResult<string>
+            {
+                PredictedInput = p.PredictedInput,
+                Similarity = p.Similarity,
+                SDR = p.SDR
+            }).ToList();
+        }
+
+        public void ClearState()
+        {
+            _knnClassifier.ClearState();
+        }
+
+        public int[] ReconstructInput(List<PredictedResult<string>> predictedResults)
+        {
+            return predictedResults.First().SDR;
+        }
+    }
+}
